c7514d7a83165e058e4f860085b65fddd3a32d4d
EnlightenedMint <70814400+EnlightenedMint@users.noreply.github.com>
2023-03-06 18:01:27 -0500

    Add files via upload

---
+++ Modules/480-Utils/480-Utils.psm1
@@ -0,0 +1,73 @@
+function 480Banner {
+    Write-Host "!!!480-Utils!!!"
+}
+
+function 480Connect([string] $server){
+
+    $conn = $global:DefaultVIServer
+    #Are we already connected?
+    if ($conn){
+        $msg = "Already connected to: {0}" -f $conn
+
+        Write-Host -ForegroundColor Green $msg
+    }
+    else {
+        $conn = Connect-VIServer -Server $server
+        #If this fails, let Connect-VIServer handle the exception
+    }
+    
+}
+
+function Get-480Config([string] $config_path) {
+    Write-Host "Reading " $config_path
+    $conf = $null
+    if (Test-Path $config_path){
+        $conf = (Get-Content -Raw -Path $config_path | ConvertFrom-Json)
+        $msg = "Using Configuration at {0}" -f $config_path
+        Write-Host -ForegroundColor "Green" $msg
+
+    }
+    else {
+        Write-Host -ForegroundColor "Yellow" "No Configuration"
+    }
+    return $conf
+    
+}
+
+function Select-VM([string] $folder){
+    $selected_vm = $null
+    try 
+    {
+        $vms = Get-VM -Location $folder
+        $index = 1
+        foreach($vm in $vms){
+            Write-Host [$index] $vm.name
+            $index+=1
+    }
+    $pick_index = Read-Host "Which index number [X] do you wish to pick?"
+    #480-TODO need to deal with an invalid index (consider making this a check function)
+    $selected_vm = $vms[$pick_index -1]
+    Write-Host "You Picked " $selected_vm.name
+    #Note this is a full on vm object we can interact with
+    return $selected_vm
+}
+    catch {
+        Write-Host "Invalid Folder: $folder" -ForegroundColor "Red"
+    }
+}
+
+
+function Cloner(){
+    $vm = Read-Host -Prompt "Enter name of VM you want to clone: "
+    $snapshot = Get-Snapshot  -VM $vm -Name "Base"
+    $vmhost = Read-Host -Prompt "Enter the ESXI IP"
+    $vmhostselected = Get-VMHost -Name $vmhost
+    $datastoresl = Read-Host -Prompt "Enter the name of the datastore"
+    $ds = Get-Datastore -Name $datastoresl
+    $linkedclone = "{0}.linked" -f $vm.name
+    $linkedvm = New-VM -LinkedClone -Name $linkedclone -VM $vm -ReferenceSnapshot $snapshot -VMHost $vmhostselected -DataStore $ds
+    $newvmmei = Read-Host "Name the new VM: "
+    $newvm = New-VM -Name $newvmmei -VM $linkedvm -VMHost $vmhostselected -DataStore $ds
+    $linkedvm | Remove-VM
+
+}
