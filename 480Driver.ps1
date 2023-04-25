4099a3a344ff5b1357b76b1431cc8e43f9c3ea08
EnlightenedMint <70814400+EnlightenedMint@users.noreply.github.com>
2023-04-25 18:50:16 -0400

    Update for module 9

--- 480Driver.ps1
+++ 480Driver.ps1
@@ -2,15 +2,19 @@ Import-Module '480-Utils' -Force
 #Call the Banner function
 480Banner
 #$conf = Get-480Config -config_path = "/home/admin/Documents/480.json"
-#480Connect -server $conf.vcenter_server
+#480Connect -server vcenter.bryan.local
 #Write-Host "Selecting your VM"
 #Select-VM -folder "BASEVM"
 #Cloner
 #SwitchCreation -SwitchName "Blue17" -PortName "BluePort17"
-Get-VMInfo -VMName "ubuntu-2"
-#StartVM -VMNamae "Rocky-*"
+#Get-VMInfo -VMName "BlueDC1"
+#StartVM -VMNamae "BlueDC1"
 #SetNetAdapter -vmname 'blue1-fw' -newnetwork '480-WAN'
-#for ($i = 1; $i -le 2; $i++) {
-#    ClonerV2 -sourcevm "ubuntu-SERVER" -destvm "ubuntu-$i" -destnetwork "BluePort17"
+#for ($i = 1; $i -le 3; $i++) {
+#    ClonerV2 -sourcevm "ubuntu-SERVER" -destvm "UbuntuAns-$i" -destnetwork "BluePort17"
 #}
-#EditVM -vmname "Rocky-3"
+#ClonerV2 -sourcevm "Win10" -destvm "BlueWKS1" -destnetwork "BluePort17"
+#EditVM -vmname "UbuntuAns-1"
+#$GuestPassword = Read-Host "Enter the password" -AsSecureString
+#$clearpass = [System.Net.NetworkCredential]::new('',$GuestPassword).Password
+#SetWinIP -VMName "BlueDC1" -GuestUser "deployer" -Guestpass $clearpass -IP "10.0.5.5" -Subnet "255.255.255.0" -Gateway "10.0.5.2" -Nameserver "10.0.5.2"
